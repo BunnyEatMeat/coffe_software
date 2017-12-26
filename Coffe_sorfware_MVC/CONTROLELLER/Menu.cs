86e138b3346e12abdec0b00c6c932d37a43d007c
DESKTOP-KKHKH84\Rio.Rai <hixbunny02@gmail.com>
2017-12-26 17:46:01 +0700

    Menu, MenuController, Connector By Duy Anhg

---
+++ Coffe_sorfware_MVC/CONTROLELLER/Menu.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CONTROLELLER
+{
+    public class Menu
+    {
+        public int ID { get; set; }
+        public string tenMon { get; set; }
+        public int gia { get; set; }
+        public int loai { get; set; }
+
+        public Menu()
+        {
+            this.ID = 0;
+            this.tenMon = null;
+            this.gia = 0;
+            this.loai = 0;
+        }
+    }//end
+}
---
+++ Coffe_sorfware_MVC/MODEL/Connector.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.SqlClient;
+using System.Data;
+
+namespace MODEL
+{
+    public class Connector
+    {
+        string strConn = "";
+        SqlConnection cn = null;
+        public Connector()
+        {
+            cn = new SqlConnection(strConn);
+        }
+
+        public void connect()
+        {
+            try
+            {
+                if (cn.State == ConnectionState.Closed)
+                    cn.Open();
+            }catch(SqlException ex)
+            { 
+                throw ex;
+            }
+        }
+
+        public void disconnect()
+        {
+            try
+            {
+                if (cn.State == ConnectionState.Open)
+                    cn.Close();
+            }catch(SqlException ex)
+            { 
+                throw ex;
+            }
+        }
+
+    }//end
+}
